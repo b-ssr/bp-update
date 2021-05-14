f3d4a32cc8fc42d99901b6a38419fcfe3a47f0a3
b-ssr <artsiom.luhin@gmail.com>
2021-05-14 13:03:54 +0200

    file upload

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const fileUpload = require('express-fileupload');
 const DBConnector = require('./database/db.js')
 
 const port = 3003;
@@ -8,29 +10,72 @@ const app = express();
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(fileUpload());
 
+
+let DB_FILE = null;
+const UPLOAD_PATH = __dirname + '/upload/';
 
 app.get('/', (req, res) => {
-    res.render('index.html');
+    clear_upload();
+    res.render('in.html');
 });
 
-app.get('/go', (req, res) => {
-    // const db_conn = new DBConnector('./database/source/gent-1.db');
-    const db_conn = new DBConnector('./database/source/IDE-gf.db');
 
-    db_conn.query().then((data) => {
-        console.log('done!');
-        res.send(data);
-    }, (err) => {
-        console.log('Error!', err.message);
-        // TODO add error handler
-        // if (err && err.code === 'QLITE_ERROR') {
-        //     console.log('Some SQL problem occurred. Check your database please.');
-        // }
+app.post('/upload', (req, res) => {
+    DB_FILE = req.files.dbFile;
+
+    if (!fs.existsSync(UPLOAD_PATH)){
+        fs.mkdirSync(UPLOAD_PATH);
+    }
+    const filePath = UPLOAD_PATH + DB_FILE.name;
+
+    DB_FILE.mv(filePath, function(err) {
+        if (err) {
+            // TODO show error message
+            console.log('UPLOAD ERROR!');
+            res.send('UPLOAD ERROR: ' + error.message);
+            // res.redirect('/');
+        } else {
+            res.redirect('/chart');
+        }
     });
-
 });
+
+
+app.get('/chart', (req, res) => {
+    if (DB_FILE) {
+        const db = new DBConnector(UPLOAD_PATH + DB_FILE.name);
+
+        db.query().then((data) => {
+            res.render('chart.html', { data: JSON.stringify(data) });
+        }).catch((error) => {
+            // TODO show error message
+            console.log('DATABASE ERROR!');
+            res.send('DATABASE ERROR: ' + error.message);
+            // res.redirect('/');
+        });
+    } else {
+        res.redirect('/');
+    }
+});
+
 
 app.get('/favicon.ico', (req, res) => res.sendStatus(204))
+
+
+function clear_upload() {
+    fs.readdir(UPLOAD_PATH, (err, files) => {
+        if (files) {
+            files.forEach(file => {
+                fs.unlink(UPLOAD_PATH + file, (error) => {
+                    if (!error) {
+                        console.log("Database file '" + file + "' was deleted from /upload directory.");
+                    }
+                });
+            });
+        }
+    });
+}
 
 app.listen(port);
